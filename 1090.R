f2c6d4b0f78ce2a8b762da8b13dff7e9a498a22b
Claudia1642 <100391974+Claudia1642@users.noreply.github.com>
2022-06-30 21:50:21 -0500

    Add files via upload

---
+++ 1090.R
@@ -0,0 +1,24 @@
+# Grupo conformado por : Diego villaizan - Claudia Garzón 
+
+# Ejercicios Módulo 1
+# Punto 1. Vectores y listas
+#Responda las siguientes preguntas:
+
+
+
+# a. ¿Cuál es la principal diferencia entre los vectores creados con la función c() y las listas creadas con la función list()?.
+
+# Resuesta: Al llamar dicha funcion c(), estamos creando un vectror, asignandole elementos o datos los cuales deseamos combinar. 
+# una de las caracteristicas es que los datos deben ser son de un mismo tipo, ya sean: mumeric,logical,integer o complex, entre otros.
+# Ejemplos: 
+Edad <- c(15,18,20,25)
+Nombre <- c("Diego","Claudia","Carlos")
+Estudios <-c( "primaria","bachillerato","pregrado","posgrado","especializacion")
+
+# Mientras tanto  con las listas creadas con la función list(), podemos combinar diferentes tipos de elementos como: vectrores,funciones
+# incluso crear una lista dentro de otra lista.
+# Ejemplo:
+lista_1 <- list(18,TRUE,"Fernando",10:30)
+#la cual automaticamente categoriza los elementos de la lista 
+
+
